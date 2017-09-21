1d74f6ec43d225326d5918bcbd7da1ad89193848
Kladky <kladky@users.noreply.github.com>
2017-09-21 12:42:23 -0400

    added resume file

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -7,7 +7,7 @@ export default () => (
     <p>I'm a software engineer. Javascript is my language of choice, and I love all things front end. I'm most familiar with the "NERD Stack" (Node.js, Express, React, Databases using SQL) but I'm a fast learner and pick up new technologies with ease!</p>
     <p>In the past, I was a technical projects manager at <a href="https://www.ted.com/">TED</a> and worked as a freelance front end developer. I've attended the <a href="https://www.gracehopper.com/">Grace Hopper immersive software engineering program</a> at <a href="https://www.fullstackacademy.com/">Fullstack Academy</a>. I have a BA from <a href="http://www.bennington.edu/">Bennington College</a> where I studied code in a visual art context.</p>
     <p>I live in Brooklyn, NY. Besides programming, I enjoy fiber arts, DIY projects, and science fiction.</p>
-    <p><Link to="/projects">Check out my projects</Link> or <a href="#">download my résumé.</a></p>
+    <p><Link to="/projects">Check out my projects</Link> or <a href="/KladkyResume.pdf">download my résumé.</a> I'm currently looking for frontend roles in the NYC area, and I appreciate any leads!</p>
     <p id="icons">
       <a href="https://twitter.com/kladky"><i className="fa fa-twitter"></i></a>
       <a href="https://www.instagram.com/kladky/"><i className="fa fa-instagram"></i></a>
